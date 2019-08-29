15221da16db4e06554f02eb5677e816db2bce3d9
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-29 18:50:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-  
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  arra.sort
 end
 
 def using_reverse(array)
-
+  a
 end
 
 def using_first(array)
